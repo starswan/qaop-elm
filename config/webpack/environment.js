a4bad51d58ecac87069e8f62046fe425b29d5eac
starswan <github@starswan.com>
2024-02-26 19:56:43 +0000

    shackapacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,7 @@
 // work around breaking change in Node 17.x
 process.env.NODE_OPTIONS = "--openssl-legacy-provider"
 
-const { environment } = require('shakapacker')
+const { environment, webpackConfig } = require('shakapacker')
 const elm =  require('./loaders/elm')
 
 environment.loaders.prepend('elm', elm)
