310beeb5e3e04cd20511bd7d39e66ad8f716c500
starswan <github@starswan.com>
2024-10-15 22:17:10 +0100

    LD H,L and LD H,A

--- app/javascript/Group0x60.elm
+++ app/javascript/Group0x60.elm
@@ -4,15 +4,13 @@ import CpuTimeCTime exposing (addCpuTimeTime)
 import Dict exposing (Dict)
 import Z80Delta exposing (Z80Delta(..), delta_noop)
 import Z80Rom exposing (Z80ROM)
-import Z80Types exposing (IXIY, IXIYHL(..), Z80, get_h, get_l, hl_deref_with_z80, set_h, set_h_ixiy, set_l)
+import Z80Types exposing (IXIY, IXIYHL(..), Z80, get_h, get_l, get_l_ixiy, hl_deref_with_z80, set_h, set_h_ixiy, set_l)
 
 
 delta_dict_60 : Dict Int (IXIYHL -> Z80ROM -> Z80 -> Z80Delta)
 delta_dict_60 =
     Dict.fromList
-        [ ( 0x65, execute_0x65 )
-        , ( 0x66, execute_0x66 )
-        , ( 0x67, execute_0x67 )
+        [ ( 0x66, execute_0x66 )
         , ( 0x68, execute_0x68 )
         , ( 0x69, execute_0x69 )
         , ( 0x6A, execute_0x6A )
@@ -30,6 +28,8 @@ miniDict60 =
         , ( 0x61, ld_h_c )
         , ( 0x62, ld_h_d )
         , ( 0x63, ld_h_e )
+        , ( 0x65, ld_h_l )
+        , ( 0x67, ld_h_a )
         ]
 
 
@@ -91,14 +91,14 @@ ld_h_e ixiyhl rom z80 =
     MainRegsWithPc value z80.pc
 
 
-execute_0x65 : IXIYHL -> Z80ROM -> Z80 -> Z80Delta
-execute_0x65 ixiyhl rom z80 =
+ld_h_l : IXIY -> Z80ROM -> Z80 -> Z80Delta
+ld_h_l ixiyhl rom z80 =
     -- case 0x65: HL=HL&0xFF|(HL&0xFF)<<8; break;
     -- case 0x65: xy=xy&0xFF|(xy&0xFF)<<8; break;
     --z80 |> set_h_z80 (get_l ixiyhl z80.main) ixiyhl
     let
         value =
-            z80.main |> set_h (get_l ixiyhl z80.main) ixiyhl
+            z80.main |> set_h_ixiy (get_l_ixiy ixiyhl z80.main) ixiyhl
     in
     MainRegsWithPc value z80.pc
 
@@ -118,14 +118,14 @@ execute_0x66 ixiyhl rom48k z80 =
     MainRegsWithPcAndCpuTime (main |> set_h value.value HL) value.pc (value.time |> addCpuTimeTime 3)
 
 
-execute_0x67 : IXIYHL -> Z80ROM -> Z80 -> Z80Delta
-execute_0x67 ixiyhl rom z80 =
+ld_h_a : IXIY -> Z80ROM -> Z80 -> Z80Delta
+ld_h_a ixiyhl rom z80 =
     -- case 0x67: HL=HL&0xFF|A<<8; break;
     -- case 0x67: xy=xy&0xFF|A<<8; break;
     --z80 |> set_h_z80 z80.flags.a ixiyhl
     let
         value =
-            z80.main |> set_h z80.flags.a ixiyhl
+            z80.main |> set_h_ixiy z80.flags.a ixiyhl
     in
     MainRegsWithPc value z80.pc
 
