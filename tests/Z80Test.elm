4a06ab772c8fa9cab3f722f13a86dff2687a5ac1
starswan <github@starswan.com>
2024-12-11 21:28:21 +0000

    Simple Z80 Address without mem access

--- tests/Z80Test.elm
+++ tests/Z80Test.elm
@@ -3,6 +3,7 @@ module Z80Test exposing (..)
 import Expect exposing (Expectation)
 import Test exposing (..)
 import Z80 exposing (execute_instruction)
+import Z80Address exposing (fromInt, toInt)
 import Z80Env exposing (mem, setMem)
 import Z80Rom
 
@@ -17,7 +18,7 @@ suite =
             Z80.constructor
 
         z80 =
-            { old_z80 | pc = addr }
+            { old_z80 | pc = addr |> fromInt }
 
         flags =
             z80.flags
@@ -46,10 +47,9 @@ suite =
                                 { z80
                                     | env = new_env
                                     , flags = { flags | a = 0x02 }
-                                    , main = { z80main | hl = 0x6545 }
                                 }
                     in
-                    Expect.equal ( addr + 1, 0x04 ) ( new_z80.pc, new_z80.flags.a )
+                    Expect.equal ( addr + 1, 0x04 ) ( new_z80.pc |> toInt, new_z80.flags.a )
             ]
         , describe "0xB8 - -xBF CP"
             [ test "0xBC CP H greater" <|
@@ -59,12 +59,12 @@ suite =
                             execute_instruction z80rom
                                 { z80
                                     | env = z80env |> setMem addr 0xBC
-                                    , main = { z80main | hl = 0x0245 }
+                                    , main = { z80main | hl = 0x0245 |> fromInt }
                                     , flags = { flags | a = 0x06 }
                                 }
                     in
                     Expect.equal { pc = addr + 1, fa = 6, fb = -3, ff = 4, fr = 4 }
-                        { pc = new_z80.pc, fa = new_z80.flags.fa, fb = new_z80.flags.fb, ff = new_z80.flags.ff, fr = new_z80.flags.fr }
+                        { pc = new_z80.pc |> toInt, fa = new_z80.flags.fa, fb = new_z80.flags.fb, ff = new_z80.flags.ff, fr = new_z80.flags.fr }
             , test "0xBC CP H less" <|
                 \_ ->
                     let
@@ -72,12 +72,12 @@ suite =
                             execute_instruction z80rom
                                 { z80
                                     | env = z80env |> setMem addr 0xBC
-                                    , main = { z80main | hl = 0x0645 }
+                                    , main = { z80main | hl = 0x0645 |> fromInt }
                                     , flags = { flags | a = 0x02 }
                                 }
                     in
                     Expect.equal { pc = addr + 1, fa = 2, fb = -7, ff = -44, fr = 252 }
-                        { pc = new_z80.pc, fa = new_z80.flags.fa, fb = new_z80.flags.fb, ff = new_z80.flags.ff, fr = new_z80.flags.fr }
+                        { pc = new_z80.pc |> toInt, fa = new_z80.flags.fa, fb = new_z80.flags.fb, ff = new_z80.flags.ff, fr = new_z80.flags.fr }
             , test "0xBC CP H equal" <|
                 \_ ->
                     let
@@ -85,11 +85,11 @@ suite =
                             execute_instruction z80rom
                                 { z80
                                     | env = z80env |> setMem addr 0xBC
-                                    , main = { z80main | hl = 0x0645 }
+                                    , main = { z80main | hl = 0x0645 |> fromInt }
                                     , flags = { flags | a = 0x06 }
                                 }
                     in
                     Expect.equal { pc = addr + 1, fa = 6, fb = -7, ff = 0, fr = 0 }
-                        { pc = new_z80.pc, fa = new_z80.flags.fa, fb = new_z80.flags.fb, ff = new_z80.flags.ff, fr = new_z80.flags.fr }
+                        { pc = new_z80.pc |> toInt, fa = new_z80.flags.fa, fb = new_z80.flags.fb, ff = new_z80.flags.ff, fr = new_z80.flags.fr }
             ]
         ]
