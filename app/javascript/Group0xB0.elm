88235acb3fb86e33bd16ef79004790ff37160dfd
starswan <github@starswan.com>
2025-04-10 16:53:46 +0100

    Fix up B0 instruction set

--- app/javascript/Group0xB0.elm
+++ app/javascript/Group0xB0.elm
@@ -30,7 +30,7 @@ or_h ixiyhl _ z80 =
     -- case 0xB4: or(HL>>>8); break;
     -- case 0xB4: or(xy>>>8); break;
     --z80 |> set_flag_regs (z80_or (get_h ixiyhl z80.main) z80.flags)
-    z80.flags |> z80_or (get_h_ixiy ixiyhl z80.main) |> FlagRegs
+    FlagRegsWithPc (z80.flags |> z80_or (get_h_ixiy ixiyhl z80.main)) z80.pc
 
 
 or_l : IXIY -> Z80ROM -> Z80 -> Z80Delta
@@ -38,7 +38,7 @@ or_l ixiyhl _ z80 =
     -- case 0xB5: or(HL&0xFF); break;
     -- case 0xB5: or(xy&0xFF); break;
     --z80 |> set_flag_regs (z80_or (get_l ixiyhl z80.main) z80.flags)
-    z80.flags |> z80_or (get_l_ixiy ixiyhl z80.main) |> FlagRegs
+    FlagRegsWithPc (z80.flags |> z80_or (get_l_ixiy ixiyhl z80.main)) z80.pc
 
 
 or_indirect_hl : IXIYHL -> Z80ROM -> Z80 -> Z80Delta
@@ -60,7 +60,7 @@ cp_h ixiyhl _ z80 =
     -- case 0xBC: cp(HL>>>8); break;
     -- case 0xBC: cp(xy>>>8); break;
     --z80 |> set_flag_regs (cp (get_h ixiyhl z80.main) z80.flags)
-    z80.flags |> z80_cp (get_h_ixiy ixiyhl z80.main) |> FlagRegs
+    FlagRegsWithPc (z80.flags |> z80_cp (get_h_ixiy ixiyhl z80.main)) z80.pc
 
 
 cp_l : IXIY -> Z80ROM -> Z80 -> Z80Delta
@@ -68,7 +68,7 @@ cp_l ixiyhl _ z80 =
     -- case 0xBD: cp(HL&0xFF); break;
     -- case 0xBD: cp(xy&0xFF); break;
     --z80 |> set_flag_regs (cp (get_l ixiyhl z80.main) z80.flags)
-    z80.flags |> z80_cp (get_l_ixiy ixiyhl z80.main) |> FlagRegs
+    FlagRegsWithPc (z80.flags |> z80_cp (get_l_ixiy ixiyhl z80.main)) z80.pc
 
 
 cp_indirect_hl : IXIYHL -> Z80ROM -> Z80 -> Z80Delta
---
+++ tests/GroupB0Test.elm
@@ -0,0 +1,394 @@
+module GroupB0Test exposing (..)
+
+import Expect exposing (Expectation)
+import Test exposing (..)
+import Z80 exposing (execute_instruction)
+import Z80Env exposing (setMem)
+import Z80Rom
+
+
+suite : Test
+suite =
+    let
+        addr =
+            30000
+
+        old_z80 =
+            Z80.constructor
+
+        z80 =
+            { old_z80 | pc = addr }
+
+        flags =
+            z80.flags
+
+        z80env =
+            z80.env
+
+        z80main =
+            z80.main
+
+        z80rom =
+            Z80Rom.constructor
+    in
+    describe "Z80.execute_instruction"
+        -- Nest as many descriptions as you like.
+        [ describe "0xB4 OR H"
+            [ test "OR H" <|
+                \_ ->
+                    let
+                        new_env =
+                            z80env
+                                |> setMem addr 0xB4
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0x03 }
+                                    , main = { z80main | hl = 0x5180 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 1, a = 0x53 } { pc = new_z80.pc, a = new_z80.flags.a }
+            , test "OR IXH" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xDD
+                                |> setMem (addr + 1) 0xB4
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0x03 }
+                                    , main = { z80main | ix = 0x5152, d = 0x60, e = 0x00, b = 0x00, c = 0x05 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 2, a = 0x53 } { pc = new_z80.pc, a = new_z80.flags.a }
+            , test "OR IYH" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xFD
+                                |> setMem (addr + 1) 0xB4
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0x03 }
+                                    , main = { z80main | iy = 0x5152, d = 0x60, e = 0x00, b = 0x00, c = 0x05 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 2, a = 0x53 } { pc = new_z80.pc, a = new_z80.flags.a }
+            ]
+        , describe "0xB5 OR L"
+            [ test "OR L" <|
+                \_ ->
+                    let
+                        new_env =
+                            z80env
+                                |> setMem addr 0xB5
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0xF0 }
+                                    , main = { z80main | hl = 0x3053 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 1, a = 0xF3 } { pc = new_z80.pc, a = new_z80.flags.a }
+            , test "OR IXL" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xDD
+                                |> setMem (addr + 1) 0xB5
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0xF0 }
+                                    , main = { z80main | ix = 0x5053 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 2, a = 0xF3 } { pc = new_z80.pc, a = new_z80.flags.a }
+            , test "OR IYL" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xFD
+                                |> setMem (addr + 1) 0xB5
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0xF0 }
+                                    , main = { z80main | iy = 0x2053, d = 0x60, e = 0x00, b = 0x00, c = 0x05 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 2, a = 0xF3 } { pc = new_z80.pc, a = new_z80.flags.a }
+            ]
+        , describe "0xB6 OR (HL)"
+            [ test "OR (HL)" <|
+                \_ ->
+                    let
+                        new_env =
+                            z80env
+                                |> setMem addr 0xB6
+                                |> setMem 0x5050 0x11
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0x76 }
+                                    , main = { z80main | hl = 0x5050, d = 0x60, e = 0x00, b = 0x00, c = 0x05 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 1, a = 0x77 } { pc = new_z80.pc, a = new_z80.flags.a }
+            , test "0xDD 0xB6 0x01 OR (IX + n)" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xDD
+                                |> setMem (addr + 1) 0xB6
+                                |> setMem (addr + 2) 0xFF
+                                |> setMem 0x5051 0x11
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0x76 }
+                                    , main = { z80main | ix = 0x5052, d = 0x60, e = 0x00, b = 0x00, c = 0x05 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 3, a = 0x77 } { pc = new_z80.pc, a = new_z80.flags.a }
+            , test "0xFD 0xB6 0x01 OR (IY + n)" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xFD
+                                |> setMem (addr + 1) 0xB6
+                                |> setMem (addr + 2) 0x01
+                                |> setMem 0x5051 0x11
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0x76 }
+                                    , main = { z80main | iy = 0x5050, d = 0x60, e = 0x00, b = 0x00, c = 0x05 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 3, a = 0x77 } { pc = new_z80.pc, a = new_z80.flags.a }
+            ]
+        , describe "0xBC CP H"
+            [ test "CP H" <|
+                \_ ->
+                    let
+                        new_env =
+                            z80env
+                                |> setMem addr 0xBC
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0x30 }
+                                    , main = { z80main | hl = 0x3053 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 1, fr = 0x00 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            , test "CP IXH" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xDD
+                                |> setMem (addr + 1) 0xBC
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0x30 }
+                                    , main = { z80main | ix = 0x3053 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 2, fr = 0x00 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            , test "CP IYH" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xFD
+                                |> setMem (addr + 1) 0xBC
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0x30 }
+                                    , main = { z80main | iy = 0x3053 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 2, fr = 0x00 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            ]
+        , describe "0xBD CP L"
+            [ test "CP L" <|
+                \_ ->
+                    let
+                        new_env =
+                            z80env
+                                |> setMem addr 0xBD
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0xF0 }
+                                    , main = { z80main | hl = 0x53F0 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 1, fr = 0x0 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            , test "CP IXL" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xDD
+                                |> setMem (addr + 1) 0xBD
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0x30 }
+                                    , main = { z80main | ix = 0x5330 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 2, fr = 0x00 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            , test "CP IYL" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xFD
+                                |> setMem (addr + 1) 0xBD
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0x30 }
+                                    , main = { z80main | iy = 0x5330, d = 0x60, e = 0x00, b = 0x00, c = 0x05 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 2, fr = 0x00 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            ]
+        , describe "0xBE CP (HL)"
+            [ test "CP (HL)" <|
+                \_ ->
+                    let
+                        new_env =
+                            z80env
+                                |> setMem addr 0xBE
+                                |> setMem 0x5050 0x11
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0x11 }
+                                    , main = { z80main | hl = 0x5050 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 1, fr = 0 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            , test "0xDD 0xBE 0x01 CP (IX + n)" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xDD
+                                |> setMem (addr + 1) 0xBE
+                                |> setMem (addr + 2) 0xFF
+                                |> setMem 0x5051 0x11
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0x11 }
+                                    , main = { z80main | ix = 0x5052 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 3, fr = 0x0 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            , test "0xFD 0xBE 0x01 CP (IY + n)" <|
+                \_ ->
+                    let
+                        alt =
+                            z80.alt_main
+
+                        new_env =
+                            z80env
+                                |> setMem addr 0xFD
+                                |> setMem (addr + 1) 0xBE
+                                |> setMem (addr + 2) 0x01
+                                |> setMem 0x5051 0x11
+
+                        new_z80 =
+                            execute_instruction z80rom
+                                { z80
+                                    | env = { new_env | sp = 0xFF77 }
+                                    , flags = { flags | a = 0x11 }
+                                    , main = { z80main | iy = 0x5050 }
+                                }
+                    in
+                    Expect.equal { pc = addr + 3, fr = 0x00 } { pc = new_z80.pc, fr = new_z80.flags.fr }
+            ]
+        ]
