f1a6d2319f0738c7b5e3a4252809b04311cd4138
starswan <github@starswan.com>
2025-04-10 17:35:05 +0100

    move tests

--- tests/GroupE0Test.elm
+++ tests/GroupE0Test.elm
@@ -30,26 +30,7 @@ suite =
     in
     describe "Z80.execute_instruction"
         -- Nest as many descriptions as you like.
-        [ describe "RST nn"
-            [ test "0xDF RST 18" <|
-                \_ ->
-                    let
-                        new_env =
-                            z80env
-                                |> setMem addr 0xDF
-                                |> setMem 0xFF75 0x16
-                                |> setMem 0xFF76 0x56
-
-                        new_z80 =
-                            execute_instruction z80rom
-                                { z80
-                                    | env = { new_env | sp = 0xFF77 }
-                                    , main = { z80main | hl = 0x5050, d = 0x60, e = 0x00, b = 0x00, c = 0x05 }
-                                }
-                    in
-                    Expect.equal { pc = 0x18, sp = 0xFF75, mem = addr + 1 } { sp = new_z80.env.sp, pc = new_z80.pc, mem = mem16 0xFF75 z80rom new_z80.env |> .value }
-            ]
-        , describe "16 bit Pop"
+        [ describe "16 bit Pop"
             [ test "POP HL (0xE1)" <|
                 \_ ->
                     let
