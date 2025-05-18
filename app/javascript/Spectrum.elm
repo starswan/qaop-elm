61e6ec680bc7eb95d086768c9e1c2af67092962d
starswan <github@starswan.com>
2025-05-18 07:45:45 +0100

    wip

--- app/javascript/Spectrum.elm
+++ app/javascript/Spectrum.elm
@@ -1199,31 +1199,6 @@ doLoad cpu z80rom tape =
 
     else
         let
-            initial =
-                { p = tape.tapePos.position
-                , ix = cpu.main.ix
-                , de = cpu.main |> get_de
-                , h = cpu.main.hl |> shiftRightBy 8
-                , l = cpu.main.hl |> Bitwise.and 0xFF
-                , a = cpu.flags.a
-                , f = cpu.flags |> get_flags
-                , rf = -1
-                , data = Dict.empty
-                , break = False
-                }
-
-            p =
-                tape.tapePos
-
-            bool =
-                case tape.tapfiles |> Dict.get p.tapfileNumber of
-                    Just aTapfile ->
-                        let
-                            tapeBlk =
-                                aTapfile.block.dataLength
-
-                            startState =
-                                initial
             initial : TapeState
             initial =
                 { p = tape.tapePos.position
@@ -1237,15 +1212,25 @@ doLoad cpu z80rom tape =
                 , data = Dict.empty
                 , break = False
                 , tapeBlk = 0
+                }
+
+            p =
+                tape.tapePos
+
+            bool =
+                case tape.tapfiles |> Dict.get p.tapfileNumber of
+                    Just aTapfile ->
+                        let
+                            tapeBlk =
+                                aTapfile.block.dataLength
+
                             startState =
                                 { initial | tapeBlk = tapeBlk }
 
                             --loadFunc =
                             --    loadOne aTapfile
-
                             --tape_data =
                             --    Loop.while (\x -> not x.break) loadFunc startState
-
                             new_data =
                                 --		for(;;) {
                                 aTapfile.block.data
