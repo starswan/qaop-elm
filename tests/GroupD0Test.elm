a949ef503d645e1bd767096b94200cd33c98f64f
Stephen Dicks <github@starswan.com>
2025-06-12 08:25:21 +0100

    Loaded Football Manager ! (#474)

--- tests/GroupD0Test.elm
+++ tests/GroupD0Test.elm
@@ -147,6 +147,42 @@ suite =
                             }
                 in
                 Expect.equal { pc = 0x10, sp = 0xFF75 } { sp = new_z80.env.sp, pc = new_z80.pc }
+        , describe "0xDC CALL C"
+            [ test "Dont jump" <|
+                \_ ->
+                    let
+                        new_env =
+                            z80env
+                                |> setMem addr 0xDC
+                                |> setMem (addr + 1) 0x05
+                                |> setMem (addr + 2) 0x34
+
+                        new_z80 =
+                            executeCoreInstruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0x39, ff = 0x0200 }
+                                }
+                    in
+                    Expect.equal ( addr + 3, 0xF765 ) ( new_z80.pc, new_z80.env.sp )
+            , test "Jump" <|
+                \_ ->
+                    let
+                        new_env =
+                            z80env
+                                |> setMem addr 0xDC
+                                |> setMem (addr + 1) 0x05
+                                |> setMem (addr + 2) 0x34
+
+                        new_z80 =
+                            executeCoreInstruction z80rom
+                                { z80
+                                    | env = new_env
+                                    , flags = { flags | a = 0x39, ff = 0x0100 }
+                                }
+                    in
+                    Expect.equal ( 0x3405, 0xF763 ) ( new_z80.pc, new_z80.env.sp )
+            ]
 
         --, test "0xD9 EXX" <|
         --    \_ ->
