971cabf24ac030bb19fa80cb7e9f62d481e21312
starswan <github@starswan.com>
2025-07-18 21:47:40 +0100

    download file

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,27 +20,54 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     # @file = Rails.cache.fetch("games_#{@game.id}") do
 
-    zipfile = faraday.get @game.download_url
-    zip_stream = Zip::InputStream.new zipfile.body
-
-    file = nil
-    while entry = zip_stream.get_next_entry
-      # All required operations on `entry` go here.
-      if entry.filepath.split("/").last == @game.filename
-        file = entry
-        break
+    input_url = @game.download_url
+    zip_data = []
+    faraday.get(input_url) do |req|
+      req.options.on_data = Proc.new do |chunk, size|
+        zip_data  << chunk
       end
     end
-    send_data file.get_input_stream.read
+    zipdata = zip_data.join
+    zipfile = StringIO.new zipdata
+
+    data = nil
+    Zip::InputStream.open(zipfile) do |zip_stream|
+      while (entry = zip_stream.get_next_entry)
+        entry_filename = entry.name.split("/").last
+        # if entry.name.ends_with?(".tap")
+        if entry_filename == @game.filename
+          data = entry.get_input_stream.read
+          break
+          # File.open("#{output_directory}/#{entry_filename}", "wb+") do |file|
+          #   file.write(data)
+          # end
+        end
+      end
+    end
+
+
+    # zipfile = faraday.get @game.download_url
+    # zip_stream = Zip::InputStream.new zipfile
+
+    # file = nil
+    # while entry = zip_stream.get_next_entry
+    #   # All required operations on `entry` go here.
+    #   if entry.filepath.split("/").last == @game.filename
+    #     file = entry
+    #     break
+    #   end
+    # end
+    # send_data file.get_input_stream.read
+    send_data data
   end
 
   private
 
   def faraday
-      Faraday.new do |f|
-        f.response :raise_error
-        f.response :follow_redirects
-        f.adapter Faraday.default_adapter
-      end
+    Faraday.new do |f|
+      f.response :raise_error
+      f.response :follow_redirects
+      f.adapter Faraday.default_adapter
+    end
   end
 end
