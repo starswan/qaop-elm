245e98cfc6cc9d6233c8679e4267d245cf2087cd
starswan <github@starswan.com>
2025-07-23 21:50:09 +0100

    fix up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,24 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import elmPlugin from 'vite-plugin-elm'
 
+if (process.env.RAILS_ENV !== 'test' && (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'arthur')) {
+    elm = elmPlugin({
+        optimize: false, // no `--optimize` option when using elm-optimize-level-2
+        nodeElmCompilerOptions: {
+            pathToElm: 'node_modules/elm-optimize-level-2/bin/elm-optimize-level-2'
+        }
+    })
+} else {
+    elm = elmPlugin({optimize: false})
+}
+
 export default defineConfig({
   build: {
     assetsInlineLimit: 24576
   },
   plugins: [
     RubyPlugin(),
-      elmPlugin({
-          optimize: false, // no `--optimize` option when using elm-optimize-level-2
-          nodeElmCompilerOptions: {
-              pathToElm: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'arthur' ? 'node_modules/elm-optimize-level-2/bin/elm-optimize-level-2' : undefined
-          }
-      })
+    elm
   ],
 })
 
