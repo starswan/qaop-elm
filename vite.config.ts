260a60cb1565b6f37eef62ad431c0b4ff37c7b42
starswan <github@starswan.com>
2025-07-26 09:39:39 +0100

    remove optimise level 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import elmPlugin from 'vite-plugin-elm'
 
-if (process.env.RAILS_ENV !== 'test' && (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'arthur')) {
-    elm = elmPlugin({
-        optimize: false, // no `--optimize` option when using elm-optimize-level-2
-        nodeElmCompilerOptions: {
-            pathToElm: 'node_modules/elm-optimize-level-2/bin/elm-optimize-level-2',
-            processOpts: "--optimize-speed"
-        }
-    })
-} else {
-    elm = elmPlugin({optimize: false})
-}
+// if (process.env.RAILS_ENV !== 'test' && (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'arthur')) {
+//     elm = elmPlugin({
+//         optimize: false, // no `--optimize` option when using elm-optimize-level-2
+//         nodeElmCompilerOptions: {
+//             pathToElm: 'node_modules/elm-optimize-level-2/bin/elm-optimize-level-2',
+//             processOpts: "--optimize-speed"
+//         }
+//     })
+// } else {
+//     elm = elmPlugin({optimize: false})
+// }
 
 export default defineConfig({
   build: {
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   plugins: [
     RubyPlugin(),
-    elm
+    elmPlugin()
   ],
 })
 
