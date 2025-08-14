174e23b72be5a19a611e1ab92dd1637e344a3551
starswan <github@starswan.com>
2025-08-14 22:32:34 +0100

    import jquery

--- app/javascript/active_admin.js
+++ app/javascript/active_admin.js
@@ -1,8 +1,9 @@
 // from https://github.com/activeadmin/activeadmin/discussions/7947
 //
-import $ from 'jquery';
+// import $ from 'jquery';
+import 'jquery';
 // window.jQuery = window.$ = $
-window.$ = $;
+// window.$ = $;
 
 // import 'jquery-ui';
 // import 'jquery-ui/ui/widgets/mouse';
