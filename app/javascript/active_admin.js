2d99b1407d94fbdc9ae82d1d04ea0e0646d81b9f
starswan <github@starswan.com>
2025-08-14 22:35:00 +0100

    convert jQuery to $

--- app/javascript/active_admin.js
+++ app/javascript/active_admin.js
@@ -1,9 +1,9 @@
 // from https://github.com/activeadmin/activeadmin/discussions/7947
 //
-// import $ from 'jquery';
-import 'jquery';
+import jQuery from 'jquery';
+// import 'jquery';
 // window.jQuery = window.$ = $
-// window.$ = $;
+window.$ = jQuery;
 
 // import 'jquery-ui';
 // import 'jquery-ui/ui/widgets/mouse';
