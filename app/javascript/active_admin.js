08d3279bceb41fe1f5ee4bcc6bea2a664faf3886
starswan <github@starswan.com>
2025-08-14 22:28:19 +0100

    remove jquery assign

--- app/javascript/active_admin.js
+++ app/javascript/active_admin.js
@@ -1,8 +1,9 @@
 // from https://github.com/activeadmin/activeadmin/discussions/7947
 //
-import $ from 'jquery'
-window.jQuery = window.$ = $
+import $ from 'jquery';
+// window.jQuery = window.$ = $
+window.$ = $;
 
-import 'jquery-ui'
-import 'jquery-ui/ui/widgets/mouse'
-import '@activeadmin/activeadmin'
+import 'jquery-ui';
+import 'jquery-ui/ui/widgets/mouse';
+import '@activeadmin/activeadmin';
