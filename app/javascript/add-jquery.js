9763727af47f257459320f0c1379d24b7f6fa37d
starswan <github@starswan.com>
2025-08-15 07:32:31 +0100

    move jquery-ui

--- app/javascript/add-jquery.js
+++ app/javascript/add-jquery.js
@@ -1,6 +1,7 @@
 // https://medium.com/@venkatramanbehera1/how-to-add-jquery-in-rails-7-application-d46fa6bcd72a
 import jquery from 'jquery';
-import 'jquery-ui';
 
 window.jQuery = jquery;
 window.$ = jquery;
+
+import 'jquery-ui';
