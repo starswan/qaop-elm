958ec3274346211b5149a44abc94eb3efe44ef9c
starswan <github@starswan.com>
2025-08-18 23:27:43 +0100

    optimise for tests

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import RubyPlugin from 'vite-plugin-ruby';
 import elmPlugin from 'vite-plugin-elm';
 
-const isBuild = (process.env.RAILS_ENV !== 'test' && (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'arthur'))
-// const isBuild= (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'arthur')
+// const isBuild = (process.env.RAILS_ENV !== 'test' && (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'arthur'))
+const isBuild= (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'arthur')
 
 const elmOptions = isBuild ? {
         // optimize: false, // no `--optimize` option when using elm-optimize-level-2
