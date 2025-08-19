9137834030469c4ee07fa0271141400766fb4c51
starswan <github@starswan.com>
2025-08-19 20:13:28 +0100

    Capistrano 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,85 +1,125 @@
-#
-# $Id$
-#
-# RVM bootstrap
-#
-require "capistrano/ext/multistage"
-set :stages, %w(alice arthur dell)
-set :default_stage, "arthur"
-set :linked_dirs, fetch(:linked_dirs, []).push('games')
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.19.2"
 
-# main details
 set :application, "retroelm"
 
-# server details
-set :deploy_via, :copy
-set :use_sudo, false
-
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 if ENV.key? "BRANCH"
-  set :scm, :git
-  set :repository, "git@github.com:starswan/qaop-elm.git"
-  # set :repository, "https://github.com/starswan/qaop-elm.git"
+#   set :scm, :git
+  set :repo_url, "git@github.com:starswan/qaop-elm.git"
   set :branch, ENV.fetch("BRANCH")
 else
-  # repo details
-  set :scm, :subversion
-  set :repository, "http://arthur/svn/starswan/trunk/projects/retro/retroelm"
+#   # repo details
+#   set :scm, :subversion
+#   set :repository, "http://arthur/svn/starswan/trunk/projects/retro/retroelm"
 end
 
-# runtime dependencies
-# depend :remote, :gem, "bundler", ">=1.0.0.rc.2"
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
 
-# tasks
-namespace :deploy do
-  task :start, roles: :app do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-  task :stop, roles: :app do
-    # Do nothing.
-  end
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
-  desc "Restart Application"
-  task :restart, roles: :app do
-    run "touch #{current_path}/tmp/restart.txt"
-    # Wakey wakey monit, work to do...
-    run "monit reload"
-  end
+# Default value for :pty is false
+# set :pty, true
 
-  desc "Symlink shared resources on each release"
-  task :symlink_shared, roles: :app do
-    # run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    fetch(:linked_files, []).each do |f|
-      run "ln -nfs #{shared_path}/#{f} #{release_path}/#{f}"
-    end
-    fetch(:linked_dirs, []).each do |f|
-      run "ln -nfs #{shared_path}/#{f} #{release_path}/public/#{f}"
-    end
-  end
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", 'config/master.key'
 
-  desc "Copy Undebug module"
-  task :undebug, roles: :app do
-    run "cd #{release_path}/app/javascript && cp ../helpers/Undebug.elm Z80Debug.elm"
-  end
-end
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
 
-after "deploy:update_code", "deploy:symlink_shared"
-after "bundler:install", "deploy:undebug"
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-namespace :bundler do
-  desc "Symlink bundled gems on each release"
-  task :symlink_bundled_gems, roles: :app do
-    run "mkdir -p #{shared_path}/bundled_gems"
-    run "ln -nfs #{shared_path}/bundled_gems #{release_path}/vendor/bundle"
-  end
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
 
-  desc "Install for production"
-  task :install, roles: :app do
-    run "cd #{release_path} && bundle config set without 'development test'"
-    run "cd #{release_path} && bundle install"
-  end
-end
+# Default value for keep_releases is 5
+# set :keep_releases, 5
 
-after "deploy:update_code", "bundler:symlink_bundled_gems"
-before "deploy:assets:precompile", "bundler:install"
-after "deploy:update_code", "deploy:migrate"
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+# require "capistrano/ext/multistage"
+# set :stages, %w(alice arthur dell)
+# set :default_stage, "arthur"
+# set :linked_dirs, fetch(:linked_dirs, []).push('games')
+#
+# # server details
+# set :deploy_via, :copy
+# set :use_sudo, false
+#
+# if ENV.key? "BRANCH"
+#   set :scm, :git
+#   set :repository, "git@github.com:starswan/qaop-elm.git"
+#   # set :repository, "https://github.com/starswan/qaop-elm.git"
+#   set :branch, ENV.fetch("BRANCH")
+# else
+#   # repo details
+#   set :scm, :subversion
+#   set :repository, "http://arthur/svn/starswan/trunk/projects/retro/retroelm"
+# end
+#
+# # runtime dependencies
+# # depend :remote, :gem, "bundler", ">=1.0.0.rc.2"
+#
+# # tasks
+# namespace :deploy do
+#   task :start, roles: :app do
+#     run "touch #{current_path}/tmp/restart.txt"
+#   end
+#
+#   task :stop, roles: :app do
+#     # Do nothing.
+#   end
+#
+#   desc "Restart Application"
+#   task :restart, roles: :app do
+#     run "touch #{current_path}/tmp/restart.txt"
+#     # Wakey wakey monit, work to do...
+#     run "monit reload"
+#   end
+#
+#   desc "Symlink shared resources on each release"
+#   task :symlink_shared, roles: :app do
+#     # run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+#     fetch(:linked_files, []).each do |f|
+#       run "ln -nfs #{shared_path}/#{f} #{release_path}/#{f}"
+#     end
+#     fetch(:linked_dirs, []).each do |f|
+#       run "ln -nfs #{shared_path}/#{f} #{release_path}/public/#{f}"
+#     end
+#   end
+#
+#   desc "Copy Undebug module"
+#   task :undebug, roles: :app do
+#     run "cd #{release_path}/app/javascript && cp ../helpers/Undebug.elm Z80Debug.elm"
+#   end
+# end
+#
+# after "deploy:update_code", "deploy:symlink_shared"
+# after "bundler:install", "deploy:undebug"
+#
+# namespace :bundler do
+#   desc "Symlink bundled gems on each release"
+#   task :symlink_bundled_gems, roles: :app do
+#     run "mkdir -p #{shared_path}/bundled_gems"
+#     run "ln -nfs #{shared_path}/bundled_gems #{release_path}/vendor/bundle"
+#   end
+#
+#   desc "Install for production"
+#   task :install, roles: :app do
+#     run "cd #{release_path} && bundle config set without 'development test'"
+#     run "cd #{release_path} && bundle install"
+#   end
+# end
+#
+# after "deploy:update_code", "bundler:symlink_bundled_gems"
+# before "deploy:assets:precompile", "bundler:install"
+# after "deploy:update_code", "deploy:migrate"
--- config/deploy/arthur.rb
+++ config/deploy/arthur.rb
@@ -1,61 +1,81 @@
-#
-# $Id$
-#
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
 
-# role :app, %w{deploy@example.com}
-# role :web, %w{deploy@example.com}
+# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
+
+
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-require "rvm/capistrano"
 
-set :deploy_to, "#{ENV['HOME']}/retroelm"
-set :user, ENV["USER"]
-# Try to speed up ruby compilation on Raspberry Pi 2
-# set :rvm_install_ruby_threads, 5
-set :rails_env, "arthur"
-set :rvm_ruby_string, "3.3.9@retroelm"
-# RVM now installed on arthur via an apt package
-# not on Debian it's not
-set :rvm_type, "/usr/share/rvm"
 
-set :linked_files, fetch(:linked_files, []) << ".env.arthur"
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
 
-# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-server "arthur.broadband", :web, :app, :db, primary: true
 
 # Custom SSH Options
 # ==================
 # You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
 #
 # Global options
 # --------------
 #  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    keys: %w(/home/user_name/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
 #
-# And/or per server (overrides global)
+# The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
+# server "example.com",
+#   user: "user_name",
 #   roles: %w{web app},
 #   ssh_options: {
-#     user: 'user_name', # overrides user setting above
+#     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
 #     forward_agent: false,
 #     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
+#     # password: "please use keys"
 #   }
+server "arthur.broadband",
+       user: ENV.fetch("USER"),
+       roles: %w{web app db},
+       deploy_to: "#{ENV['HOME']}/retroelm"
+
+# require "rvm/capistrano"
+#
+# set :deploy_to, "#{ENV['HOME']}/retroelm"
+# set :user, ENV["USER"]
+# # Try to speed up ruby compilation on Raspberry Pi 2
+# # set :rvm_install_ruby_threads, 5
+# set :rails_env, "arthur"
+# set :rvm_ruby_string, "3.3.9@retroelm"
+# # RVM now installed on arthur via an apt package
+# # not on Debian it's not
+# set :rvm_type, "/usr/share/rvm"
+#
+# set :linked_files, fetch(:linked_files, []) << ".env.arthur"
+#
+# server "arthur.broadband", :web, :app, :db, primary: true
