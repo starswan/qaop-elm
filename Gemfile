0422bab61ec5a367226ac6e3dc1bb38f94d6ef63
Stephen Dicks <github@starswan.com>
2025-08-20 12:48:49 +0100

    Capistrano 3 (#575)

--- Gemfile
+++ Gemfile
@@ -73,15 +73,17 @@ group :development do
   # gem 'spring'
   # gem 'spring-watcher-listen', '~> 2.0.0'
 
+  gem 'bcrypt_pbkdf'
   # Use Capistrano for deployment
-  # gem "capistrano", "~> 2.15.8"
-  gem "capistrano"
-  gem "capistrano-ext"
+  gem "capistrano", ">=3"
+  gem "capistrano-bundler"
+  gem "capistrano-passenger", require: false
   gem "capistrano-rails"
-  gem "rvm-capistrano", require: false
+  gem "capistrano-rvm", require: false
 
   gem 'ed25519'
-  gem 'bcrypt_pbkdf'
+  # required for passenger support
+  gem 'rexml'
 end
 
 group :test do
--- config/deploy/alice.rb
+++ config/deploy/alice.rb
@@ -1,61 +1,77 @@
-#
-# $Id$
-#
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
 
-# role :app, %w{deploy@example.com}
-# role :web, %w{deploy@example.com}
+# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
+
+
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-require "rvm/capistrano"
 
-set :deploy_to, "/home/starswan/html/retro"
-set :user, "starswan"
-set :rvm_ruby_string, "3.3.9@retroelm"
-# Try to speed up ruby compilation on Raspberry Pi 2
-# set :rvm_install_ruby_threads, 5
-set :default_environment,
-    "BASH_ENV" => "$HOME/.bashrc",
-    "http_proxy" => "http://localhost:8888/",
-    "https_proxy" => "http://localhost:8888/"
 
-# set :linked_files, fetch(:linked_files, []) << ".env.production"
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
 
-# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-server "alice", :web, :app, :db, primary: true
 
 # Custom SSH Options
 # ==================
 # You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
 #
 # Global options
 # --------------
 #  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    keys: %w(/home/user_name/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
 #
-# And/or per server (overrides global)
+# The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
+# server "example.com",
+#   user: "user_name",
 #   roles: %w{web app},
 #   ssh_options: {
-#     user: 'user_name', # overrides user setting above
+#     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
 #     forward_agent: false,
 #     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
+#     # password: "please use keys"
 #   }
+server "alice",
+        user: "starswan",
+        roles: %w{web app db}
+
+set :deploy_to, "/home/starswan/html/retro"
+set :rails_env, "production"
+set :rvm_ruby_version, "3.3.9@retroelm"
+# # Try to speed up ruby compilation on Raspberry Pi 2
+# # set :rvm_install_ruby_threads, 5
+# set :default_environment,
+#     "BASH_ENV" => "$HOME/.bashrc",
+#     "http_proxy" => "http://localhost:8888/",
+#     "https_proxy" => "http://localhost:8888/"
+#
+# server "alice", :web, :app, :db, primary: true
+#
