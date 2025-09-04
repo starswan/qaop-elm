eb1ea5f64c0cd224af935f698948811b8f53e6ff
starswan <github@starswan.com>
2025-09-04 14:56:29 +0100

    try with 2 terser passes

--- vite.config.mjs
+++ vite.config.mjs
@@ -48,6 +48,7 @@ export default defineConfig(({ mode }) => {
             assetsInlineLimit: 24576,
             terserOptions: {
                 compress: {
+                    passes: 2,
                     pure_funcs: ['F2','F3','F4','F5','F6','F7','F8','F9','A2','A3','A4','A5','A6','A7','A8','A9'],
                     pure_getters: true,
                     keep_fargs: false,
