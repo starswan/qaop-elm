b1fed7acd33e6a89d839274cc0240047a0cc1e62
Stephen Dicks <github@starswan.com>
2025-10-11 11:45:15 +0100

    More games (#648)

--- test/factories/games.rb
+++ test/factories/games.rb
@@ -1,10 +1,34 @@
 FactoryBot.define do
   factory :game do
-    filetype {  "tap" }
+    filetype {  "TAP" }
 
     trait :match_day do
-      name { 'Match Day' }
-      tapfile { 'MATCHDAY.tap' }
+      name { 'MatchDay' }
+      download_url { "https://www.worldofspectrum.org/pub/sinclair/games/m/MatchDay.tap.zip"}
+      filetype { "TAP" }
+      filename { 'MATCHDAY.TAP' }
+    end
+
+    # Sadly this TAP file appears to be broken
+    # trait :chuckie_egg do
+    #   name { "ChuckieEgg" }
+    #   download_url { "https://www.worldofspectrum.org/pub/sinclair/games/c/ChuckieEgg.tap.zip" }
+    #   filetype { "tap" }
+    #   filename { "Chuckie Egg.tap" }
+    # end
+
+    trait :colossus do
+      name { "ColossusChess" }
+      download_url { "https://www.worldofspectrum.org/pub/sinclair/games/c/ColossusChess4.tap.zip" }
+      filetype { "TAP" }
+      filename { "COLOSSUS.TAP" }
+    end
+
+    trait :checkers do
+      name { "Checkers" }
+      download_url { "https://www.worldofspectrum.org/pub/sinclair/games/c/Checkers.tap.zip" }
+      filetype { "tap" }
+      filename { "checkers.tap" }
     end
 
     trait :manic_miner do
@@ -33,21 +57,25 @@ FactoryBot.define do
       name { 'Regs' }
       download_url { "https://github.com/raxoft/z80test/releases/download/v1.2a/z80test-1.2a.zip" }
       filename { 'z80doc.tap' }
+      filetype { "tap" }
     end
 
     trait :z80_full_flags do
       name { 'FullFlags' }
       download_url { "https://github.com/raxoft/z80test/releases/download/v1.2a/z80test-1.2a.zip" }
       filename { 'z80flags.tap' }
+      filetype { "tap" }
     end
 
     trait :z80_test_full do
       name { 'Full' }
       download_url { "https://github.com/raxoft/z80test/releases/download/v1.2a/z80test-1.2a.zip" }
       filename { 'z80full.tap' }
+      filetype { "tap" }
     end
 
     trait :football_manager do
+      filetype { "tap" }
       name { 'Football Manager' }
       filename { 'FOOTMANG.TAP' }
       picture_url { "https://worldofspectrum.net/pub/sinclair/screens/in-game/f/FootballManager.gif" }
