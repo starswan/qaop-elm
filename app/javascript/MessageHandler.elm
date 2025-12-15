d2a22488bfc92ae676737adf6012d902356a609e
Stephen Dicks <github@starswan.com>
2025-12-15 12:02:34 +0000

    fixup bytesToRom (#704)

--- app/javascript/MessageHandler.elm
+++ app/javascript/MessageHandler.elm
@@ -1,7 +1,7 @@
 module MessageHandler exposing (..)
 
 import Bytes exposing (Bytes)
-import Http exposing (Error, Expect, Metadata, Response)
+import Http exposing (Error(..), Expect, Metadata, Response)
 import Tapfile exposing (Tapfile, parseTapFile)
 import Z80Rom exposing (Z80ROM, parseRomFile)
 
@@ -33,7 +33,7 @@ bytesToTap httpResponse =
             Ok (body |> parseTapFile)
 
 
-bytesToRom : Response Bytes -> Result Error (Maybe Z80ROM)
+bytesToRom : Response Bytes -> Result Error Z80ROM
 bytesToRom httpResponse =
     case httpResponse of
         Http.BadUrl_ url ->
@@ -49,24 +49,9 @@ bytesToRom httpResponse =
             Err (Http.BadStatus metadata.statusCode)
 
         Http.GoodStatus_ _ body ->
-            --let
-            -- would be nice to parse headers - but we seem to get a
-            -- gzipped body size not an actual size which makes things tough
-            --x = debug_log "got tap headers" metadata.headers Nothing
-            --length = metadata.headers |> Dict.get "content-length" |> Maybe.withDefault "0" |> String.toInt |> Maybe.withDefault 0
-            -- This seems to be the gzip size which isn't that useful
-            --z = debug_log "received bytes of size" (body |> width) Nothing
-            --in
-            Ok (body |> parseRomFile)
+            case body |> parseRomFile of
+                Just a ->
+                    Ok a
 
-
-
---list_decoder : Int -> Decoder Int -> Decoder (List Int)
---list_decoder size decoder =
---   loop (size, []) (listStep decoder)
---listStep : Decoder Int -> (Int, List Int) -> Decoder (Step (Int, List Int) (List Int))
---listStep decoder (n, xs) =
---   if n <= 0 then
---     succeed (Done xs)
---   else
---     map (\x -> Loop (n - 1, x :: xs)) decoder
+                Nothing ->
+                    Err (BadBody "ROM parse failed")
