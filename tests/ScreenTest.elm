ddaa732dfdcadba1005cbbf54787342b32d6a164
starswan <github@starswan.com>
2026-03-30 10:53:24 +0100

    tidy up

--- tests/ScreenTest.elm
+++ tests/ScreenTest.elm
@@ -2,6 +2,7 @@ module ScreenTest exposing (..)
 
 import Expect
 import Test exposing (..)
+import Vector8
 import Z80Screen exposing (foldBoolRunCounts)
 
 
@@ -13,7 +14,7 @@ suite =
                 \_ ->
                     let
                         a =
-                            [ 0x80, 0x03 ] |> List.concatMap Z80Screen.intToBools
+                            [ 0x80, 0x03 ] |> List.concatMap (\int -> int |> Z80Screen.bitsToLines |> Vector8.toList)
                     in
                     Expect.equal
                         [ { count = 1, value = True }
