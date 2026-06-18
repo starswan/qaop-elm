019c7adcf92651b7ba78eeaeec6b285670b0c5b4
starswan <github@starswan.com>
2026-06-18 17:46:14 +0100

    Games in rows of 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Can't persuade this to run (yet) 23rd July 2025 - Executes IM1 and then stops
-# Game.create! :name => 'Match Day', :filename => 'MATCHDAY.TAP',
-#              picture_url: "https://worldofspectrum.org/pub/sinclair/screens/in-game/m/MatchDay.gif",
-#              download_url: "https://www.worldofspectrum.org/pub/sinclair/games/m/MatchDay.tap.zip",
-#              filetype: "tap"
+# This runs ok now
+Game.create! :name => 'Match Day', :filename => 'MATCHDAY.TAP',
+             picture_url: "https://worldofspectrum.org/pub/sinclair/screens/in-game/m/MatchDay.gif",
+             download_url: "https://www.worldofspectrum.org/pub/sinclair/games/m/MatchDay.tap.zip",
+             filetype: "tap"
 
 # Runs but seems to always produce the same cards. Crashed on second run with unknown variable error
 # Game.create! :name => '8 Cards Patience' , :filename => '8CARDPAT.TAP',
