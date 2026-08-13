e7290822ce23c61818e7296b5bd82169421487ab
starswan <github@starswan.com>
2026-08-13 21:02:06 +0100

    3.3.12

--- config/deploy/alice.rb
+++ config/deploy/alice.rb
@@ -65,7 +65,7 @@ server "alice",
 
 set :deploy_to, "/home/starswan/html/retro"
 set :rails_env, "production"
-set :rvm_ruby_version, "3.3.10@retroelm"
+set :rvm_ruby_version, "3.3.12@retroelm"
 # # Try to speed up ruby compilation on Raspberry Pi 2
 # # set :rvm_install_ruby_threads, 5
 # set :default_environment,
