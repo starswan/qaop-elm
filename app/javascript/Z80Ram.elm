43252ea89a97b2afb8409a2ebcd9134ce9099e68
starswan <github@starswan.com>
2026-09-21 19:57:31 +0100

    screen banks

--- app/javascript/Z80Ram.elm
+++ app/javascript/Z80Ram.elm
@@ -7,66 +7,45 @@ import Z80MemoryDict exposing (Z80MemoryDict, getMemValue, setMemValue)
 
 type alias Z80Ram =
     { screen : Z80Screen
-    , non_screen : Z80MemoryDict
+    , ula_ram : Z80MemoryDict
+    , bank2 : Z80MemoryDict
+    , bank3 : Z80MemoryDict
     }
 
 
 constructor : Z80Ram
 constructor =
     let
-        ram =
-            List.repeat (49152 - 6912) 0
+        ula =
+            List.repeat (16384 - 6912) 0 |> Z80MemoryDict.constructor
+
+        ram2 =
+            List.repeat 16384 0 |> Z80MemoryDict.constructor
+
+        ram3 =
+            List.repeat 16384 0 |> Z80MemoryDict.constructor
     in
-    Z80Ram ScreenStorage.constructor (Z80MemoryDict.constructor ram)
+    Z80Ram ScreenStorage.constructor ula ram2 ram3
 
 
 getRamValue : Int -> Z80Ram -> Int
 getRamValue addr z80ram =
-    let
-        ram_addr =
-            addr - 6912
-    in
-    if ram_addr >= 0 then
-        z80ram.non_screen |> getMemValue ram_addr
+    if addr < 16384 then
+        let
+            ram_addr =
+                addr - 6912
+        in
+        if ram_addr >= 0 then
+            z80ram.ula_ram |> getMemValue ram_addr
+
+        else
+            z80ram.screen |> getScreenValue addr
+
+    else if addr < 32768 then
+        z80ram.bank2 |> getMemValue (addr - 16384)
 
     else
-        z80ram.screen |> getScreenValue addr
-
-
-
---getRam16Value : Int -> Z80Ram -> Int
---getRam16Value addr z80ram =
---    let
---        ram_addr =
---            addr - 6912
---
---        ram_addr1 =
---            addr + 1 - 6912
---    in
---    let
---        low =
---            if ram_addr >= 0 then
---                z80ram.non_screen |> getMemValue ram_addr
---
---            else
---                z80ram.screen |> getScreenValue addr
---
---        high =
---            if ram_addr1 >= 0 then
---                z80ram.non_screen |> getMemValue ram_addr1
---
---            else
---                z80ram.screen |> getScreenValue (addr + 1)
---    in
---    Bitwise.or low (shiftLeftBy8 high)
---setRamValue : Int -> Int -> Z80Ram -> Z80Ram
---setRamValue addr value z80ram =
---    -- This is only used when folding into the actual data at the end of a cycle
---    if addr >= 6912 then
---        { z80ram | non_screen = z80ram.non_screen |> setMemValue (addr - 6912) value }
---
---    else
---        { z80ram | screen = z80ram.screen |> setScreenValue addr value }
+        z80ram.bank3 |> getMemValue (addr - 32768)
 
 
 foldDictIntoRam : Dict Int Int -> Z80Ram -> Z80Ram
@@ -74,11 +53,21 @@ foldDictIntoRam ramdict z80_ram =
     ramdict
         |> Dict.foldl
             (\addr value z80ram ->
-                if addr >= 6912 then
-                    { z80ram | non_screen = z80ram.non_screen |> setMemValue (addr - 6912) value }
+                if addr < 16384 then
+                    let
+                        ram_addr =
+                            addr - 6912
+                    in
+                    if ram_addr >= 0 then
+                        { z80ram | ula_ram = z80ram.ula_ram |> setMemValue ram_addr value }
+
+                    else
+                        { z80ram | screen = z80ram.screen |> setScreenValue addr value }
+
+                else if addr < 32768 then
+                    { z80ram | bank2 = z80ram.bank2 |> setMemValue (addr - 16384) value }
 
                 else
-                    { z80ram | screen = z80ram.screen |> setScreenValue addr value }
-             --z80ram |> setRamValue key value
+                    { z80ram | bank3 = z80ram.bank3 |> setMemValue (addr - 32768) value }
             )
             z80_ram
